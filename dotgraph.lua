c5081c8343fd7c1a0ebcba9052f5e201165bd8f3
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-06-21 00:11:34 -0400

    Make adding source optional

--- dotgraph.lua
+++ dotgraph.lua
@@ -3,33 +3,42 @@ dotgraph - a Lua filter for pandoc to convert code blocks with class "dot" into
 SVG images using dot (Graphviz). If the code block has an attribute "caption",
 it is used as the caption, and the image object becomes a figure.
 
+If the code block has class "includeSource" (or "include-source"), a comment
+containing the dot source is included before the diagram figure.
+
 This does not use the mediabag and the --extract-media option, because that
 doesn't play well with relative paths. Instead, the produced image is written
 directly to a file.
-
-The graphviz source code is added as an HTML comment before the image.
 --]]
 
 -- A filter operating on code blocks
 function CodeBlock(elem)
-	if elem.classes[1] == "dot" then
+	if elem.classes:includes("dot") then
 		return createDotGraph(elem)
 	end
 end
 
--- Generate an SVG image using dot, return a paragraph containing a pandoc
--- image object
+-- Generate an SVG image using dot, return a list containing source code
+-- comment (if desired) and a pandoc image object
 function createDotGraph(elem)
 	local img = pandoc.pipe("dot", {"-Tsvg"}, elem.text)
 	local fname = "diagrams/" .. pandoc.sha1(img):sub(1, 7) .. ".svg"
 
 	storeImg(fname, img)
 
+	retList = pandoc.List()
+	if elem.classes:includes("includeSource") or elem.classes:includes("include-source") then
+		local comment = pandoc.RawBlock("html", "<!--\n" .. elem.text .. "\n-->")
+		retList:extend({comment})
+	end
+
 	local imgObj = getImgObj(elem, fname)
-	local comment = pandoc.RawBlock("html", "<!--\n" .. elem.text .. "\n-->")
-	return pandoc.List({comment, pandoc.Para{imgObj}})
+	retList:extend({pandoc.Para{imgObj}})
+
+	return retList
 end
 
+-- Store the image to a file in the artifacts subdirectory
 function storeImg(fname, img)
 	local f = assert(io.open("artifacts/" .. fname, "w"))
 	f:write(img)
